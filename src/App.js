59c8d14a5d6232265ad32d079938537a04920caf
J-grigri <jurgis.grigorjevs@gmail.com>
2020-03-20 21:14:48 +0700

    hardcoding no more

--- src/App.js
+++ src/App.js
@@ -3,23 +3,31 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import MovieCard from './components/MovieCard.js';
 import Spinner from 'react-bootstrap/Spinner'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Nav, NavDropdown, Form, FormControl,} from "react-bootstrap";
 let apikey = process.env.REACT_APP_JURGIS;
+// let page = 1;
+let keyword = '';
+let movieList =[];
 
 function App() {
-
-  let[movies,setMovies] = useState(null);
-  let nowPlaying = async () => {
-    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US&page=1`;
-    let data = await fetch(url);
-    let dataResult = await data.json();
-    console.log ("data", dataResult);
-
-    setMovies(dataResult.results)
+  //State
+  let[movies,setMovies] = useState()
+  let popularMovies = async () => {
+    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US&page=$1`;
+    //here we get an array of 20 movies
+    let fetchedData = await fetch(url);
+    let data = await fetchedData.json(); 
+    console.log ("This is movieList", data);
+    movieList = data.results; 
     
+    //original movieList
+    setMovies(movieList)     
   }
 
   //useEffect executes after the render
-  useEffect(nowPlaying,[]);
+  useEffect(popularMovies,[]);
+
   if (movies == null){
     return (
       <Spinner animation="border" role="status">
@@ -27,11 +35,38 @@ function App() {
       </Spinner>
     )
   }
+  //Search function for search input
+  let searchByKeyword = (e) => { 
+    keyword = e.target.value;
+    if(keyword === ''){
+      setMovies(movieList)
+    } else {
+      setMovies(movieList.filter((movie)=>movie.title.toLowerCase().includes(keyword.toLowerCase())));
+    }
+  }
   
+
   return (
-    //nowPlaying movies main section
+    //order of elements on the main page
     <div>
-      <MovieCard movieList = {movies}/>
+      <Navbar bg="dark" variant="dark" >
+        <Navbar.Brand href="#home">Movies!</Navbar.Brand>
+        <Nav className="mr-auto">
+          <NavDropdown title="Categories" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Comedies</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Documentaries</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Favorites</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange= {(e)=>searchByKeyword(e)}/>
+          <Button onClick={()=>searchByKeyword()} variant="outline-info">Search</Button>
+        </Form> 
+      </Navbar >
+      <MovieCard fetchedMovies = {movies}/>
+      <Button variant="outline-danger">More</Button>
     </div>
   );
 }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,27 +1,57 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
+import { Card, ListGroupItem, ListGroup } from "react-bootstrap";
+import moment from 'moment'
+let genre_id;
+
+
 
 export default function MovieCard(props) {
-    //find values using map and prepare for output
-    let htmlMovie = props.movieList.map((movie) => {
-        return (
-            <Card className="bg-dark text-white col-md-4">
-                <Card.Img src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} alt="Card image" />
-                <Card.ImgOverlay>
-                    <Card.Title>{movie.title}</Card.Title>
-                    <Card.Text>{movie.overview}</Card.Text>
-                    <Card.Text>Released: {movie.release_date}</Card.Text>
-                    <Card.Text>Rating: {}</Card.Text>
-                </Card.ImgOverlay>
-            </Card>
 
-        )
-    })
+    //find values using map and prepare for output
+    let htmlPopularMovies = props.fetchedMovies.map((movie) => {
+
+        //momentJS to set the time format 
+        let releaseDate = moment(movie.release_date).format("MMMM YYYY");
+        //limit the description text lenght
+        let descriptionLength = (movie.overview.split('.')[0] + ".");
+
+        let genre_id = movie.genre_ids[0];
+
+        let checkId = () => {
+            if (genre_id == "28") return " Action"
+        }
+        
+
+
+
+
+            console.log("genre id just now", genre_id)//correct
+
+            return (
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`} />
+                    <Card.Body>
+                        <Card.Title>{movie.title}</Card.Title>
+                        <Card.Text>{descriptionLength}</Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>Released: {releaseDate}</ListGroupItem>
+                        <ListGroupItem>Rating: {movie.vote_average}</ListGroupItem>
+                        <ListGroupItem>Genre id:{checkId()}</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        <Card.Link href="#">Card Link</Card.Link>
+                        <Card.Link href="#">Another Link</Card.Link>
+                    </Card.Body>
+                </Card>
+
+            )
+        });
 
     return (
         <div className="row">
-            {htmlMovie}
+            {htmlPopularMovies}
         </div>
     )
 }
